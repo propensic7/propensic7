01fb80940a803387da676279a020c21cd0f3c25c
propensic7 <auliaizen@gmail.com>
2016-04-10 21:51:25 +0700

    tes commit

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -57,6 +57,11 @@ class SiteController extends Controller
     {
         return $this->render('index');
     }
+	
+	public function actionTest()
+	{
+		return $this->render('index');
+	}
 
     public function actionLogin()
     {
